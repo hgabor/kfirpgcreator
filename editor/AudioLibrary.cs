c2b3c9793fb85b6dd84fad83b2cd690c3f2702cb
Halász Gábor <hgabor@sch.bme.hu>
2009-09-27 23:14:02 +0000

    Removed unused Audio class, will be replaced by the global large file handler.
    git-svn-id: https://kfirpgcreator.svn.sourceforge.net/svnroot/kfirpgcreator/trunk@187 de0ceee1-c12a-0410-b4aa-b6e97b09e4b8

--- editor/AudioLibrary.cs
+++ editor/AudioLibrary.cs
@@ -10,30 +10,6 @@ using WeifenLuo.WinFormsUI.Docking;
 
 namespace KFIRPG.editor {
 	partial class AudioLibrary: DockableForm {
-		class Audio {
-			string outerLocation = null;
-			public string OuterLocation {
-				get { return outerLocation; }
-				set {
-					outerLocation = value;
-					if (outerLocation != null) {
-						innerName = System.IO.Path.GetFileName(outerLocation);
-					}
-				}
-			}
-			string innerName = null;
-			public string InnerName {
-				get { return innerName; }
-				set { innerName = value; }
-			}
-
-			public override string ToString() {
-				if (outerLocation == null && innerName == null) return "::Invalid::";
-				else if (outerLocation == null) return innerName;
-				else return string.Format("{0} ({1})", innerName, outerLocation);
-			}
-		}
-
 		public AudioLibrary() {
 			InitializeComponent();
 		}
