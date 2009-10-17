4941a4b475f22938cf7dca0e9c4bc7f14c4b1eb9
Halász Gábor <hgabor@sch.bme.hu>
2009-10-17 23:08:53 +0000

    Undo/redo done for sprite deletion. Issue: 64
    git-svn-id: https://kfirpgcreator.svn.sourceforge.net/svnroot/kfirpgcreator/branches/Undo@200 de0ceee1-c12a-0410-b4aa-b6e97b09e4b8

--- editor/Cursors/DeleteSpriteCursor.cs
+++ editor/Cursors/DeleteSpriteCursor.cs
@@ -4,8 +4,27 @@ using System.Text;
 
 namespace KFIRPG.editor.Cursors {
 	class DeleteSpriteCursor: Cursor {
-		public override void Click(Map.Layer layer) {
-			layer.objects[tileX, tileY] = null;
+		public override string Name {
+			get {
+				return "Delete sprite(s)";
+			}
+		}
+
+		protected override void Edit(Map.Layer layer) {
+			int tileX = this.tileX;
+			int tileY = this.tileY;
+			if (tileX >= layer.objects.GetLength(0) || tileY >= layer.objects.GetLength(1)) {
+				return;
+			}
+			Map.Obj oldObj = layer.objects[tileX, tileY];
+			if (oldObj == null) return;
+			commandList.Add(new Commands.Command(
+				delegate() {
+					layer.objects[tileX, tileY] = null;
+				},
+				delegate() {
+					layer.objects[tileX, tileY] = oldObj;
+				}));
 		}
 
 		public override void Draw(System.Drawing.Graphics g) { }
