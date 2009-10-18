b2cf0d11170fc007d97797224ced828927eaa43f
Halász Gábor <hgabor@sch.bme.hu>
2009-10-18 13:06:04 +0000

    CommandList is now really a List<Command> Issue: 61
    git-svn-id: https://kfirpgcreator.svn.sourceforge.net/svnroot/kfirpgcreator/branches/Undo@201 de0ceee1-c12a-0410-b4aa-b6e97b09e4b8

--- editor/Commands/CommandList.cs
+++ editor/Commands/CommandList.cs
@@ -3,34 +3,26 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace KFIRPG.editor.Commands {
-	class CommandList {
-		List<Command> list = new List<Command>();
-
+	class CommandList: List<Command> {
 		public CommandList(string name) {
 			this.Name = name;
 		}
 
-		public void Add(Command command) {
-			list.Add(command);
-		}
-		public bool Contains(Command command) {
-			return list.Contains(command);
-		}
 		public bool IsEmpty {
-			get { return list.Count == 0; }
+			get { return this.Count == 0; }
 		}
 
 		public string Name { get; private set; }
 
 		public void Do() {
-			for (int i = 0; i < list.Count; ++i) {
-				list[i].Do();
+			for (int i = 0; i < this.Count; ++i) {
+				this[i].Do();
 			}
 		}
 
 		public void Undo() {
-			for (int i = list.Count - 1; i >= 0; --i) {
-				list[i].Undo();
+			for (int i = this.Count - 1; i >= 0; --i) {
+				this[i].Undo();
 			}
 		}
 	}
