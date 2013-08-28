d2adc72b8d3bf2ae1150a6d5c3de15e0f309dd47
Halász Gábor <hgabor@level14.hu>
2013-08-28 02:17:48 +0200

    Doc comments for the Cursor class.

--- editor/Cursors/Cursor.cs
+++ editor/Cursors/Cursor.cs
@@ -9,17 +9,38 @@ namespace KFIRPG.editor.Cursors {
 			commandList = new UndoCommandList(Name);
 		}
 
+		/// <summary>
+		/// Paints all content associated with the cursor, including the frame and preview content.
+		/// </summary>
+		/// <param name="g"></param>
 		public void Draw(System.Drawing.Graphics g) {
 			commandList.ForEach(c => PreDraw(c.X, c.Y, g));
 			DrawCursor(g);
 		}
 
+		/// <summary>
+		/// Paints the cursor frame.
+		/// </summary>
+		/// <param name="g"></param>
 		protected abstract void DrawCursor(System.Drawing.Graphics g);
+		/// <summary>
+		/// Draws a preview of the content associated with the cursor.
+		/// </summary>
+		/// <param name="x"></param>
+		/// <param name="y"></param>
+		/// <param name="g"></param>
 		protected virtual void PreDraw(int x, int y, System.Drawing.Graphics g) { }
+		/// <summary>
+		/// Adds the cursors actions to the current command list.
+		/// </summary>
+		/// <param name="layer">The layer the actions are performed on.</param>
 		protected abstract void Edit(Map.Layer layer);
 
 		public abstract string Name { get; }
 
+		/// <summary>
+		/// Fires when an action was performed with this cursor.
+		/// </summary>
 		public EventHandler<UndoEventArgs> CommandReady;
 		private void OnCommand(UndoEventArgs e) {
 			if (CommandReady != null) CommandReady(this, e);
@@ -27,20 +48,39 @@ namespace KFIRPG.editor.Cursors {
 
 		UndoCommandList commandList;
 
+		/// <summary>
+		/// Creates a command from the passed undo and redo functions, and adds it to the undo list.
+		/// </summary>
+		/// <param name="doCmd"></param>
+		/// <param name="redoCmd"></param>
 		protected void AddCommand(UndoCommand.Function doCmd, UndoCommand.Function redoCmd) {
 			UndoCommand c = new UndoCommand(x, y, tileX, tileY, doCmd, redoCmd);
 			AddCommand(c);
 		}
 
+		/// <summary>
+		/// Adds a command to the undo list.
+		/// </summary>
+		/// <param name="c"></param>
 		private void AddCommand(UndoCommand c) {
 			if (commandList.TrueForAll(o => !(o.TileX == c.TileX && o.TileY == c.TileY))) {
 				commandList.Add(c);
 			}
 		}
 
+		/// <summary>
+		/// Starts performing the actions associated with the cursor.
+		/// Some cursors can painted, thus the undo action will be finalized when the
+		/// <see cref="EndEdit()"/> function is called.
+		/// </summary>
+		/// <param name="layer">The layer the cursor should act on.</param>
 		public void DoEdit(Map.Layer layer) {
 			this.Edit(layer);
 		}
+		/// <summary>
+		/// Ends the action associated with the cursor. If it had any effect, a CommandReady
+		/// event is fired.
+		/// </summary>
 		public void EndEdit() {
 			if (commandList.IsEmpty) return;
 			OnCommand(new UndoEventArgs(commandList));
@@ -51,6 +91,13 @@ namespace KFIRPG.editor.Cursors {
 		protected int y = 0;
 		protected int tileX = 0;
 		protected int tileY = 0;
+		/// <summary>
+		/// Sets the coordinates of the cursor, usually due to mouse movement.
+		/// </summary>
+		/// <param name="x"></param>
+		/// <param name="y"></param>
+		/// <param name="tileX"></param>
+		/// <param name="tileY"></param>
 		public void UpdateCoords(int x, int y, int tileX, int tileY) {
 			this.x = x;
 			this.y = y;
