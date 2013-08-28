6f4cf9850a95e5f6ff7acd0601bef8985d27c88c
Halász Gábor <hgabor@level14.hu>
2013-08-28 02:18:31 +0200

    Added LayerGroup class to be used instead of layers.

---
+++ editor/LayerGroup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KFIRPG.editor
+{
+	/// <summary>
+	/// A group of layers. It is represented to the a user as a layer, but in reality it can contain
+	/// multiple game layers.
+	/// </summary>
+	abstract class LayerGroup
+	{
+		protected readonly Map.Layer[] layers;
+
+		protected LayerGroup(int layerCount = 1)
+		{
+			layers = new Map.Layer[layerCount];
+		}
+	}
+}
---
+++ editor/SimpleLayerGroup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KFIRPG.editor
+{
+	class SimpleLayerGroup : LayerGroup
+	{
+		public SimpleLayerGroup(Map.Layer layer)
+		{
+			base.layers[0] = layer;
+		}
+	}
+}
